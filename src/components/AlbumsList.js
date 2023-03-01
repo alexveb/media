1d7cc9c73b64669ec505baef8639c8f7d258710b
alexveb <verbalex2011@hotmail.com>
2023-03-01 21:11:04 +0200

    album api finish

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,44 @@
+import {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from "../store/apis/albumsApi";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+  const [removeAlbum, removeAlbumResults] = useRemoveAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      return <AlbumsListItem key={album.id} album={album} />;
+    });
+  }
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { albumsApi } from "./apis/albumsApi";
 import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
 
 //export everything
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+export {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from "./apis/albumsApi";
